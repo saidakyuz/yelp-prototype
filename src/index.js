d9223cf9bd4ef9183265079540bb7cf96baefda9
Maria-p92 <mariaparradesign@gmail.com>
2021-03-18 16:50:10 +0100

    merge index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import mockdata from './mock-data.json';
 
+function App() {
+  console.log(mockdata);
+    return (
+      <div className = "App">
+        <Header/>
+        Banner
+        Searchbar
+        CityFilter
 
-ReactDOM.render(
-  <React.StrictMode >
-    <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+        <CardComponent/>
+        <MapComponent/>
+
+        <DetailPage/>
+        Router
+        Landingpage
+        Detailpage
+        Router
+
+        <Footer/>
+      </div>
+    );
+}
+
+export default App;
