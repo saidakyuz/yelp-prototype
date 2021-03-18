1f625b6e211899fe38b60b5667bde5b7495a6650
Son Le Duc <jsonleduc@gmail.com>
2021-03-18 15:57:29 +0100

    first draft

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-ReactDOM.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>,
+ReactDOM.render(
+  <React.StrictMode >
+    <App />
+    </React.StrictMode>,
     document.getElementById('root')
 );
